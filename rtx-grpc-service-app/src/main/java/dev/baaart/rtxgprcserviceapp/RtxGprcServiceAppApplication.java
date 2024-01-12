7e5c663e77917c8c3cc76d904878a7d35d09deb7
Bartosz Perczynski <bartosz_p@hotmail.com>
2024-01-12 15:42:11 +0000

    Initial Commit

---
+++ rtx-grpc-service-app/src/main/java/dev/baaart/rtxgprcserviceapp/RtxGprcServiceAppApplication.java
@@ -0,0 +1,13 @@
+package dev.baaart.rtxgprcserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtxGprcServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtxGprcServiceAppApplication.class, args);
+    }
+
+}
---
+++ rtx-grpc-service-app/src/test/java/dev/baaart/rtxgprcserviceapp/RtxGprcServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.baaart.rtxgprcserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtxGprcServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
